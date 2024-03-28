dc6cd37f206eb711b737262eabbe7a55ba85376a
avonggeorge <georgeavong@yahoo.com>
2024-03-28 14:02:01 -0700

    revert code

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -4,4 +4,5 @@
 # -s = Silent mode. Don't show progress meter or error messages.
 # -L = Redirects in URL#!/bin/bash
 # This script sends a get request and also and displays the body of the response
-response=$(curl -sI $1) && [[ "$response" == *"200 OK"* ]] && curl -s $1
+
+curl -sL "$1"
