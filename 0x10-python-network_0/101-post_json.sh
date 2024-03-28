d8dd11dd79abb953905f15c26b7f077175fb4d76
avonggeorge <georgeavong@yahoo.com>
2024-03-27 18:02:58 -0700

    Task done

---
+++ 0x10-python-network_0/101-post_json.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Sends a JSON POST request to a given URL with a given JSON file.
+
+# -s = silent mode. Don't show progress meter or error messages.
+# -H = specify header content type to be application/json.
+# -d = specify HTTP POST data to post.
+# cat "$2" = Read the contents of the JSON file.
+
+curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
