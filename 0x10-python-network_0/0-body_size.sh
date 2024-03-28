95ff8b034ea5cd4b4cdb92f534d538bc74232661
avonggeorge <georgeavong@yahoo.com>
2024-03-27 18:13:29 -0700

    correction

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -4,4 +4,4 @@
 # -s = Silent mode. Don't show progress meter or error messages.
 # | = Pipe the output of the previous command to the next command.4
 # wc -c = Print the byte size of the HTTP response header.(character count)
-curl -s "$1" | wc -c
+curl -s "${1}" | wc -c
