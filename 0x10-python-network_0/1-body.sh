c8a64023a5f139c43aeaaffb6c652eb4d0b61daf
avonggeorge <georgeavong@yahoo.com>
2024-03-28 13:56:23 -0700

    code correct

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -2,5 +2,6 @@
 # Get the response body for a given URL for 200 status code responses.
 
 # -s = Silent mode. Don't show progress meter or error messages.
-# -L = Redirects in URL
-curl -sL "$1"
+# -L = Redirects in URL#!/bin/bash
+# This script sends a get request and also and displays the body of the response
+response=$(curl -sI $1) && [[ "$response" == *"200 OK"* ]] && curl -s $1
