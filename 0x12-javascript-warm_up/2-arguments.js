4843a4d3733fed3ed6aa2ac4de91840fa8a6f0b9
avonggeorge <georgeavong@yahoo.com>
2024-03-29 10:39:28 +0100

    modified code

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 // A Script that prints msgs based on the number of argument passed to it
 function checkArg(arg) {
-    if (process.argv.length === 3) {
-        console.log("Argument found");
-    } else if (process.argv.length > 3) {
-        console.log("Arguments found");
-    } else if (process.argv.length === 2) {
+    if (process.argv.length <= 2) {
         console.log("No argument");
+    } else if (process.argv.length === 3) {
+        console.log("Argument found");
+    } else {
+        console.log("Arguments found");
     }
 }
 //  Call the function
